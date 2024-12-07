b1a1525693fb5a4d0016ede502316ac6fae747a9
melissacrawford396 <melissacrawford396@gmail.com>
2024-12-07 10:11:37 -0600

    joystick returns to middle of button

--- knob.gd
+++ knob.gd
@@ -1,9 +1,10 @@
 extends Sprite2D
 
 @onready var parent = $".."
-
+@onready var joystickButton = $"../JoystickButton"
 
 var pressing = false
+var center_of_joystick
 
 
 @export var maxLength = 50 
@@ -12,21 +13,37 @@ var pressing = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	maxLength *= parent.scale.x
+	center_of_joystick = joystickButton.position + (joystickButton.size * 0.5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if pressing:
-		if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
-			global_position = get_global_mouse_position()
-		else:
-			var angle = parent.global_position.angle_to_point(get_global_mouse_position())
-			global_position.x = parent.global_position.x + cos(angle) * maxLength
-			global_position.y = parent.global_position.y + sin(angle) * maxLength
-			
+		global_position = get_global_mouse_position()
+		#if get_global_mouse_position().distance_to(parent.global_position) <= maxLength:
+			#global_position = get_global_mouse_position()
+		#else:
+			#var angle = parent.global_position.angle_to_point(get_global_mouse_position())
+			#global_position.x = parent.global_position.x + cos(angle) * maxLength
+			#global_position.y = parent.global_position.y + sin(angle) * maxLength
+		#calculateVector()
 	else:
-		global_position = lerp(global_position, parent.global_position, delta * 10)
+		
+		global_position = lerp(global_position, center_of_joystick, delta * 10)
+		#global_position = lerp(global_position, parent.global_position, delta * 10)
+		parent.posVector = Vector2(0, 0)
+		
+	#print(global_position, joystickButton.global_position)
+	#print(parent.posVector)
+
+func calculateVector():
+	if((global_position.x - parent.global_position.x) >= deadZone):
+		parent.posVector.x = (global_position.x - parent.global_position.x) / maxLength
+	if((global_position.y - parent.global_position.y) >= deadZone):
+		parent.posVector.y = (global_position.y - parent.global_position.y) / maxLength
+		
+		
 
 
 func _on_button_button_down() -> void:
